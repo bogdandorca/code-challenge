b2d18d8f07d9804296b0f8a53b890322541c689f
Bogdan Dorca <bogdandorca@Bogdans-MacBook-Pro.local>
2018-05-06 17:54:36 +0300

    Trying to fix mail2/mime issue

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const serve = require('express-static');
-const fs = require('fs');
 
 // config
 const port = process.env.PORT || 8080;
@@ -12,7 +10,7 @@ const comicsApp = require('./server/comics/comics.app');
 app.use('/api/characters', charactersApp);
 app.use('/api/comics', comicsApp);
 
-app.use(serve('./dist'));
+app.use(express.static('./dist'));
 
 app.listen(port, () => {
     console.log(`Access the most awesome app at http://localhost:${port}`);
